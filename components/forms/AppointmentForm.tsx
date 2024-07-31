5d2843a1c5c0205bf2cba2bf4a5c077970b9a4af
Dragan Subotic <dragan.subi7@gmail.com>
2024-07-31 20:31:55 +0200

    implement admin page

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -12,7 +12,8 @@ import { useRouter } from "next/navigation"
 import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
 export enum FormFieldType {
   INPUT = "input",
@@ -24,28 +25,36 @@ export enum FormFieldType {
   SKELETON = "skeleton"
 }
 
+interface AppointmentFormType {
+  userId: string;
+  patientId: string;
+  type: "create" | "cancel" | "schedule";
+  appointment: Appointment,
+  setOpen: (open: boolean) => void;
+}
+
 const AppointmentForm = ({
   userId,
   patientId,
-  type
-}: {
-  userId: string;
-  patientId: string;
-  type: "create" | "cancel" | "schedule"
-}) => {
+  type,
+  appointment,
+  setOpen
+}: AppointmentFormType) => {
 
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const AppointmentFormValidation = getAppointmentSchema(type);
+
+
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: "",
+      primaryPhysician: appointment ? appointment.primaryPhysician : "",
+      schedule: appointment ? new Date(appointment.schedule) : new Date(),
+      reason: appointment && appointment.reason,
+      note: appointment ? appointment.note : "",
+      cancellationReason: appointment?.cancellationReason || "",
     },
   })
 
@@ -56,17 +65,15 @@ const AppointmentForm = ({
 
     switch (type) {
       case "schedule":
-        status = "schedualed";
+        status = "scheduled";
         break;
       case "cancel":
-        status = "canceled";
+        status = "cancelled";
         break;
       default:
         status = "pending";
         break;
     }
-
-
 
     try {
       if (type === "create" && patientId) {
@@ -84,6 +91,25 @@ const AppointmentForm = ({
         if (appointment) {
           form.reset();
           router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
+        }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment.$id!,
+          appointment: {
+            primaryPhysician: values?.primaryPhysician,
+            schedule: new Date(values?.schedule),
+            status: status as Status,
+            cancellationReason: values?.cancellationReason
+          },
+          type
+        }
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+          setOpen && setOpen(false);
+          form.reset();
         }
       }
 
@@ -108,11 +134,12 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">New Appointment</h1>
-          <p className="text-dark-700">Request a new appointment in 10 seconds</p>
-        </section>
-
+        {type === "create" &&
+          <section className="mb-12 space-y-4">
+            <h1 className="header">New Appointment</h1>
+            <p className="text-dark-700">Request a new appointment in 10 seconds</p>
+          </section>
+        }
         {type !== "cancel" && (
           <>
             <CustomFormField
@@ -143,7 +170,7 @@ const AppointmentForm = ({
             <CustomFormField
               fieldType={FormFieldType.DATE_PICKER}
               control={form.control}
-              name="schedale"
+              name="schedule"
               label="Expected appointment date"
               showTimeSelect
               dateFormat="MM/dd/yyyy - h:mm aa "
